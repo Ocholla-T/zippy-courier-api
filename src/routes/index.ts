f524ad78123034e9a2480548424ebcd2a6652813
Ocholla-T <tonnyocholla8@gmail.com>
2022-07-01 10:14:11 +0300

    ADD: route to post request for login

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import { createUser, authenticateUser } from '@controller/user_controller'
+import passport from 'passport'
 
 const Router: express.Router = express.Router()
 
 Router.post('/users/register', createUser)
-Router.post('/users/auth', authenticateUser)
+Router.post('/users/login', authenticateUser)
+
 export default Router
